fe41db79d25f878cfb775698a083946d250205a6
Can Bogoclu <can.bogoclu@gmail.com>
2024-12-28 17:39:30 +0100

    fix: ruff errors

--- experiment_design/variable/__init__.py
+++ experiment_design/variable/__init__.py
@@ -11,3 +11,15 @@ from experiment_design.variable.space import (
     VariableCollection,
     create_correlation_matrix,
 )
+
+__all__ = [
+    "ContinuousVariable",
+    "DiscreteVariable",
+    "Variable",
+    "create_continuous_uniform_variables",
+    "create_discrete_uniform_variables",
+    "create_variables_from_distributions",
+    "ParameterSpace",
+    "VariableCollection",
+    "create_correlation_matrix",
+]
